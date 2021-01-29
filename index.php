f05023eaaa566a35b1cd83126752ff9835a3d08a
DByoujin <58168699+DByoujin@users.noreply.github.com>
2021-01-29 18:48:43 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    $path = "http://apis.data.go.kr/1741000/DisasterMsg2/getDisasterMsgList?ServiceKey=인증키&type=xml";
+    $xml = simplexml_load_file($path);
+     
+    foreach($xml->row as $message) {
+        echo "보낸시간 : ".$message->create_date."<br>";
+        echo "보낸 지역 : ".$message->location_name."<br>";
+        echo "메시지 내용 : ".$message->msg."<br><br>";
+    }
+        echo "Warning : ".$xml->MESSAGE."<br>CODE : ".$xml->CODE;
+
+?>
